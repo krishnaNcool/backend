b052d21b8f079ae3957831d7a75f99cee6eb960a
unknown <krishnannagesh81@gmail.com>
2021-12-08 15:43:08 +0530

    fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const format = require('date-format')
 const app = express()
-const port = 3000 || process.env.port
+const port = process.env.port || 300
 
 app.get("/", (req, res)=>{
     res.send("hello")
