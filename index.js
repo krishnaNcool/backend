9581cbad189c3dbea67d16adbf2a1d7bb90ab073
unknown <krishnannagesh81@gmail.com>
2021-12-08 15:25:34 +0530

    start

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const format = require('date-format')
+const app = express()
+const port = 3000 || process.env.port
+
+app.get("/", (req, res)=>{
+    res.send("hello")
+})
+
+app.get("/api/v1/instagram", (req, res)=>{
+    const instagramData={
+        user:"kris",
+        followers:10,
+        date: format.asString("dd  MM --- hh:mm:ss", new Date())
+    }
+    res.status(200).json({instagramData})
+})
+
+
+
+app.get("/api/v1/facebook", (req, res)=>{
+    const instagramData={
+        user:"krishna",
+        followers:100,
+        date: new Date()
+    }
+    res.status(200).json({instagramData})
+})
+
+// capture URL token data in params
+app.get("/api/v1/:token", (req, res)=>{
+    const instagramData={
+        user:"krishna",
+        followers:100,
+        date: new Date()
+    }
+    res.status(200).json({param: req.params.token})
+})
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
